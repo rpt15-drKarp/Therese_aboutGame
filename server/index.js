35d97b4546016d6d7f03f12eb948109a785ffb50
Therese Brown <browntm96@gmail.com>
2019-12-03 12:52:10 -0500

    use /

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const db = require('../db/mysql/mysqlConfig.js');
 const app = express();
 
 app.use('/:gameId', express.static(__dirname + '/../public'));
-app.use(express.static('public'));
+app.use('/', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
